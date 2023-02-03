9410fafcbec033fcee6b57870c7524dbf497edef
kobewi <kobewi4e@gmail.com>
2023-02-04 00:19:49 +0100

    Snake movemement

---
+++ Scripts/GameSnake.gd
@@ -0,0 +1,39 @@
+extends Node3D
+
+@onready var camera_pivot: Node3D = $CameraPivot
+@onready var camera_3d: Camera3D = %Camera3D
+@onready var snakes: Node3D = $Snakes
+
+var current_snake: Node3D
+
+func _ready() -> void:
+	update_current_snake()
+
+func _process(delta: float) -> void:
+	if Input.is_key_pressed(KEY_D):
+		camera_pivot.rotate_y(delta)
+		update_current_snake()
+	elif Input.is_key_pressed(KEY_A):
+		camera_pivot.rotate_y(-delta)
+		update_current_snake()
+	
+	assert(current_snake)
+	
+	camera_pivot.position.y = lerpf(camera_pivot.position.y, current_snake.head_position.y, 0.1)
+
+func update_current_snake():
+	var new_current: Node3D
+	
+	var min_dist := INF
+	for snake in snakes.get_children():
+		var dist = snake.global_position.distance_squared_to(camera_3d.global_position)
+		if dist < min_dist:
+			new_current = snake
+			min_dist = dist
+	
+	if new_current != current_snake:
+		if current_snake:
+			current_snake.is_current = false
+		
+		current_snake = new_current
+		new_current.is_current = true
--- Scripts/Snake.gd
+++ Scripts/Snake.gd
@@ -4,30 +4,40 @@ extends Node3D
 @onready var body: CSGPolygon3D = $CSGPolygon3D
 @onready var head: MeshInstance3D = $MeshInstance3D
 
-var direction: Vector3 = Vector3.RIGHT
+@export var up_direction: Vector3
+
+var is_current: bool
+
+var direction: Vector3 = Vector3.UP
+var head_position: Vector3
 var current_point := 1
 
 func _ready() -> void:
-#	Engine.time_scale = 0.1
-	curve.set_point_position(current_point, curve.get_point_position(current_point) + direction* 0.01)
+	head_position = direction * 0.01
+	curve.set_point_position(current_point, head_position)
 	head.position = curve.get_point_position(current_point) + direction * 0.5
 	head.rotation.y = get_forward_rotation()
 
 func _process(delta: float) -> void:
-	curve.set_point_position(current_point, curve.get_point_position(current_point) + direction * 2 * delta)
+	head_position += direction * 2 * delta
+	curve.set_point_position(current_point, head_position)
 #	head.position = head.position.lerp(curve.get_point_position(current_point) + direction * 0.8, 0.01)
-	head.position = curve.get_point_position(current_point) + direction * 0.5
+	head.position = head_position + direction * 0.5
 	head.rotation.y = lerp_angle(head.rotation.y, get_forward_rotation(), 0.1)
 	
-	if Input.is_action_just_pressed("ui_left"):
-		direction = direction.rotated(Vector3.UP, PI/2)
-		curve.add_point(curve.get_point_position(current_point) + direction * delta)
-		current_point += 1
+	if not is_current:
+		return
 	
-	if Input.is_action_just_pressed("ui_right"):
-		direction = direction.rotated(Vector3.UP, -PI/2)
-		curve.add_point(curve.get_point_position(current_point))
-		current_point += 1
+	if Input.is_action_just_pressed(&"ui_left"):
+		rotate_head(PI/2)
+	
+	if Input.is_action_just_pressed(&"ui_right"):
+		rotate_head(-PI/2)
+
+func rotate_head(angle: float):
+	direction = direction.rotated(up_direction, angle)
+	curve.add_point(head_position + direction * 0.01)
+	current_point += 1
 
 func get_forward_rotation() -> float:
 	if direction.is_equal_approx(Vector3.RIGHT):
