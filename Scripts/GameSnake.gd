d6778711354349a872e0cd28a083872744cfcbc9
John Meadow <armitage3@gazeta.pl>
2023-02-04 12:32:29 +0100

    keyboard camera tweaks

--- Scripts/GameSnake.gd
+++ Scripts/GameSnake.gd
@@ -5,16 +5,45 @@ extends Node3D
 @onready var snakes: Node3D = $Snakes
 
 var current_snake: Node3D
+var camera_height_direction = 0.0
+var camera_height_factor = 0.0
+var camera_orbit_direction = 0.0
+var camera_orbit_factor = 0.0
 
 func _ready() -> void:
 	update_current_snake()
 
 func _process(delta: float) -> void:
+
+	camera_handle_orbit(delta)
+	camera_handle_height(delta)
+
+func camera_handle_orbit(delta: float):
+	camera_orbit_direction = 0.0
 	if Input.is_key_pressed(KEY_D):
-		camera_pivot.rotate_y(delta)
+		camera_orbit_direction += delta * 3
+	if Input.is_key_pressed(KEY_A):
+		camera_orbit_direction -= delta * 3
+	camera_orbit_factor = lerp(camera_orbit_factor, camera_orbit_direction, 0.2)
+	
+	if camera_orbit_direction == 0.0 and abs(camera_orbit_factor)<0.001:
+		camera_orbit_factor = 0.0
+	else:
+		camera_pivot.rotate_y(camera_orbit_factor)
 		update_current_snake()
-	elif Input.is_key_pressed(KEY_A):
-		camera_pivot.rotate_y(-delta)
+
+func camera_handle_height(delta: float):
+	camera_height_direction = 0.0
+	if Input.is_key_pressed(KEY_W):
+		camera_height_direction += delta * 30.0
+	if Input.is_key_pressed(KEY_S):
+		camera_height_direction -= delta * 30.0
+	camera_height_factor = lerp(camera_height_factor, camera_height_direction,0.2)
+	
+	if camera_height_direction == 0 and abs(camera_height_factor)<0.01:
+		camera_height_factor = 0.0
+	else:
+		camera_pivot.position.y += camera_height_factor
 		update_current_snake()
 
 func _input(event: InputEvent) -> void:
@@ -22,9 +51,10 @@ func _input(event: InputEvent) -> void:
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
 			camera_pivot.position.y += event.factor
 			update_current_snake()
-		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN or event.button_index == KEY_S:
 			camera_pivot.position.y -= event.factor
 			update_current_snake()
+			
 
 func update_current_snake():
 	var new_current: Node3D
